46f3a2708cc18fd3cee3a772df4b452747cff638
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2020-10-14 10:27:10 -0300

    Tratamentos de erros, documentação e refatorações

--- src/jsMain/kotlin/net.notjustanna.calendar/utils.kt
+++ src/jsMain/kotlin/net.notjustanna.calendar/utils.kt
@@ -1,7 +1,10 @@
-package net.notjustanna.utils
+package net.notjustanna.calendar
 
 import org.w3c.fetch.Headers
 
+/**
+ * Utility method to create a "Headers" class by using a mapOf-like function.
+ */
 fun headers(vararg pairs: Pair<String, String>) = Headers().apply {
     for ((k, v) in pairs) append(k, v)
 }
